87106ac1ddea68f6a8598193444bedbe9913d514
cacashitoshito <cacashitoshito@gmail.com>
2022-09-12 01:18:01 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  main - Entyr point
- *  Description: prints two digits combination
- *  Return: Always 0 (success)
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int c, i;
-
+	
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
