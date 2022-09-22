3b688b575a7c4a906052d8e970bd4aa6e8dbeef1
cacashitoshito <cacashitoshito@gmail.com>
2022-09-22 03:07:20 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _strcmp - compares two strings
  * @s1: first string.
+ * @s2: second string.
  * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
